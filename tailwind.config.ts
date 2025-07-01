739caa1f10ee51522ccee48ccac2e19bef584974
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-01 14:58:23 +0530

    Fix: Tailwind content paths for production

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,11 +3,11 @@ import type { Config } from "tailwindcss"
 const config: Config = {
   darkMode: ["class"],
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./app/**/*.{js,ts,jsx,tsx}"
+     "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  "./src/components/**/*.{js,ts,jsx,tsx}",
+  "./src/pages/**/*.{js,ts,jsx,tsx}",
+  "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     container: {
