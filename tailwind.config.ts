c8e822c141e324327d87a964dc25420db12e1949
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-01 15:11:15 +0530

    fix: update Tailwind config and rebuild styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,7 +7,6 @@ const config: Config = {
   "./src/**/*.{js,ts,jsx,tsx}",
   "./src/components/**/*.{js,ts,jsx,tsx}",
   "./src/pages/**/*.{js,ts,jsx,tsx}",
-  "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     container: {
