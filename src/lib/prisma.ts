3a04943cc36d1fe9d665dbe247fbc9314dd774b0
Antonypoulraj <aaddy1998apj@gmail.com>
2025-07-07 11:23:32 +0530

    update:package.jsn and tsconfig.json configuration in the git

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,12 @@
-// src/lib/prisma.ts
+// File: src/lib/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ?? new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
 
